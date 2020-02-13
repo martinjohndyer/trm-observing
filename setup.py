a5a08fb799457957635b3b654482530001c8d601
Tom Marsh <t.r.marsh@warwick.ac.uk>
2020-02-13 16:45:14 +0000

    updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='trm.observing',
-      version='1.0.0',
+      version='1.0.1',
       packages = ['trm', 'trm.observing'],
       scripts=['scripts/eplanner.py', 'scripts/parallactic.py'],
 
